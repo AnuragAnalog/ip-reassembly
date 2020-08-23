a9b24389dae7adf6cf39a6bcc7fb5e8ed315bc8f
AnuragAnalog <anurag.peddi1998@gmail.com>
2020-08-23 21:11:52 +0530

    added template

---
+++ utils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+class parser():
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+class datagram():
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+class reassembly():
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
